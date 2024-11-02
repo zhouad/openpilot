e8ffc89331f42d82d7d9896c3c3beba5cf6082bd
moumou758 <moumou758@163.com>
2024-11-02 11:06:33 +0800

    Update test_monitoring.py

--- selfdrive/monitoring/test_monitoring.py
+++ selfdrive/monitoring/test_monitoring.py
@@ -7,11 +7,11 @@ from openpilot.selfdrive.monitoring.helpers import DriverMonitoring, DRIVER_MONI
 EventName = car.CarEvent.EventName
 dm_settings = DRIVER_MONITOR_SETTINGS()
 
-TEST_TIMESPAN = 240  # seconds
-DISTRACTED_SECONDS_TO_ORANGE = dm_settings._DISTRACTED_TIME - dm_settings._DISTRACTED_PROMPT_TIME_TILL_TERMINAL + 31
-DISTRACTED_SECONDS_TO_RED = dm_settings._DISTRACTED_TIME + 31
-INVISIBLE_SECONDS_TO_ORANGE = dm_settings._AWARENESS_TIME - dm_settings._AWARENESS_PROMPT_TIME_TILL_TERMINAL + 31
-INVISIBLE_SECONDS_TO_RED = dm_settings._AWARENESS_TIME + 31
+TEST_TIMESPAN = 120  # seconds
+DISTRACTED_SECONDS_TO_ORANGE = dm_settings._DISTRACTED_TIME - dm_settings._DISTRACTED_PROMPT_TIME_TILL_TERMINAL + 1
+DISTRACTED_SECONDS_TO_RED = dm_settings._DISTRACTED_TIME + 1
+INVISIBLE_SECONDS_TO_ORANGE = dm_settings._AWARENESS_TIME - dm_settings._AWARENESS_PROMPT_TIME_TILL_TERMINAL + 1
+INVISIBLE_SECONDS_TO_RED = dm_settings._AWARENESS_TIME + 1
 
 def make_msg(face_detected, distracted=False, model_uncertain=False):
   ds = log.DriverStateV2.new_message()
@@ -20,8 +20,8 @@ def make_msg(face_detected, distracted=False, model_uncertain=False):
   ds.leftDriverData.faceProb = 1. * face_detected
   ds.leftDriverData.leftEyeProb = 1.
   ds.leftDriverData.rightEyeProb = 1.
-  ds.leftDriverData.leftBlinkProb = 0. # Always not distracted
-  ds.leftDriverData.rightBlinkProb = 0. # Always not distracted
+  ds.leftDriverData.leftBlinkProb = 1. * distracted
+  ds.leftDriverData.rightBlinkProb = 1. * distracted
   ds.leftDriverData.faceOrientationStd = [1.*model_uncertain, 1.*model_uncertain, 1.*model_uncertain]
   ds.leftDriverData.facePositionStd = [1.*model_uncertain, 1.*model_uncertain]
   # TODO: test both separately when e2e is used
