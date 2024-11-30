97bed5dd770b639962f70213b393b8a65e59e68b
moumou758 <moumou758@163.com>
2024-11-30 21:43:43 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export ATHENA_HOST='ws://mr-one.cn:7899'
-export API_HOST='http://mr-one.cn:7898'
+export ATHENA_HOST='ws://1.15.136.221:7899'
+export API_HOST='http://1.15.136.221:7898'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
 exec ./launch_chffrplus.sh
