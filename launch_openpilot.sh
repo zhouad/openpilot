99acf8f576b4612674a427cfa7a658a93454683b
moumou758 <moumou758@163.com>
2025-01-05 19:24:55 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export ATHENA_HOST='ws://1.15.136.221:7899'
-export API_HOST='http://1.15.136.221:7898'
+export ATHENA_HOST='wss://athena.konik.ai/'
+export API_HOST='https://api.konik.ai'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
 exec ./launch_chffrplus.sh
