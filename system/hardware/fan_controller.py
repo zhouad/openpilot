16d0cebc47558498968d4d61f50e688a72232139
moumou758 <moumou758@163.com>
2025-03-22 11:38:14 +0800

    Update fan_controller.py

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -32,9 +32,9 @@ class TiciFanController(BaseFanController):
                       #error=error,
                       #feedforward=interp(cur_temp, [60.0, 100.0], [0, -100])
                     #))
-    fan_pwr_out = int(interp(cur_temp, [60.0, 80.0], [0, 75]))
+    fan_pwr_out = int(interp(cur_temp, [60.0, 80.0], [0, 80]))
 
-    # 确保风扇功率在 0 到 75 之间
-    fan_pwr_out = max(0, min(75, fan_pwr_out))
+    # 确保风扇功率在 0 到 80 之间
+    fan_pwr_out = max(0, min(80, fan_pwr_out))
     self.last_ignition = ignition
     return fan_pwr_out
