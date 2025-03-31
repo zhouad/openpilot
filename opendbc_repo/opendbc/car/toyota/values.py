f91696153b29e4482b85bb0aa3f0bf3c552532bc
Rick Lan <ricklan@gmail.com>
2025-03-31 13:45:33 +0800

    Toyota SDSU - 2025/05/27

--- opendbc_repo/opendbc/car/toyota/values.py
+++ opendbc_repo/opendbc/car/toyota/values.py
@@ -55,11 +55,14 @@ class ToyotaSafetyFlags(IntFlag):
   STOCK_LONGITUDINAL = (2 << 8)
   LTA = (4 << 8)
   SECOC = (8 << 8)
+  LONG_FILTER = (16 << 8)
 
 
 class ToyotaFlags(IntFlag):
   # Detected flags
   HYBRID = 1
+  # use legacy id
+  SDSU = 2
   DISABLE_RADAR = 4
 
   # Static flags
