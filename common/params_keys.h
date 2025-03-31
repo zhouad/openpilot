caaf48dd1a94bea1a5b605138d97faa27a43ae6c
Rick Lan <ricklan@gmail.com>
2025-03-31 13:37:45 +0800

    Toyota TSS1 SnG - init

--- common/params_keys.h
+++ common/params_keys.h
@@ -137,4 +137,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"dp_device_audible_alert_mode", CLEAR_ON_MANAGER_START},
     {"dp_lon_no_gas_gating", PERSISTENT},
     {"dp_device_auto_shutdown_in", PERSISTENT},
+    {"dp_toyota_tss1_sng", PERSISTENT},
 };
