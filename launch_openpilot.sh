9969d78e799c1180625c21357acd6b5c10add850
moumou758 <moumou758@163.com>
2025-04-03 19:27:13 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export ATHENA_HOST='ws://laolang.duckdns.org:7899'
-export API_HOST='http://laolang.duckdns.org:7898'
+export ATHENA_HOST='wss://athena.konik.ai'
+export API_HOST='https://api.konik.ai'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
 exec ./launch_chffrplus.sh
