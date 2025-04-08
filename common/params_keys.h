e7dd630639c6ee08f64a41cbe7ef0c1f1453aac9
Rick Lan <ricklan@gmail.com>
2025-04-08 18:10:55 +0800

    On/Off Road - init

--- common/params_keys.h
+++ common/params_keys.h
@@ -119,4 +119,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"dp_device_last_log", CLEAR_ON_MANAGER_START},
     {"dp_device_reset_conf", CLEAR_ON_MANAGER_START},
+    {"dp_device_go_off_road", CLEAR_ON_MANAGER_START},
 };
