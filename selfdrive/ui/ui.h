145d6d71237e7043be3b275906d72a385a60af25
Rick Lan <ricklan@gmail.com>
2025-04-08 22:45:08 +0800

    O3 - 2025/05/14

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -60,6 +60,7 @@ typedef struct UIScene {
   float light_sensor = -1;
   bool started, ignition, is_metric;
   uint64_t started_frame;
+  bool disable_driver = false;
 } UIScene;
 
 class UIState : public QObject {
--- system/manager/manager.py
+++ system/manager/manager.py
@@ -41,6 +41,9 @@ def manager_init() -> None:
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
     ("DisableLogging", "0"),
+    ("dp_device_is_rhd", "0"),
+    ("dp_device_monitoring_disabled", "0"),
+    ("dp_device_beep", "0"),
   ]
 
   if params.get_bool("RecordFrontLock"):
