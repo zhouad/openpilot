zuowenjing/dragonpilot

model selector - init

common/params_keys.h
@@ -117,4 +117,6 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"UpdaterLastFetchTime", PERSISTENT},
     {"Version", PERSISTENT},
+    {"dp_general_model_selected", PERSISTENT},
+    {"dp_general_model_list", PERSISTENT},
 };

zhouad/openpilot

common/params_keys.h
@@ -119,4 +119,6 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"dp_device_last_log", CLEAR_ON_MANAGER_START},
     {"dp_device_reset_conf", CLEAR_ON_MANAGER_START},
+    {"dp_device_model_selected", PERSISTENT},
+    {"dp_device_model_list", PERSISTENT},
 };