2c7cbadd062cd2dd0797075e1a0b11e216d2b676
Rick Lan <ricklan@gmail.com>
2025-04-16 22:51:14 +0800

    Audiable Alert Mode - init

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -41,6 +41,7 @@ def manager_init() -> None:
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
     ("DisableLogging", "0"),
+    ("dp_device_audible_alert_mode", "0"),
   ]
 
   if params.get_bool("RecordFrontLock"):
