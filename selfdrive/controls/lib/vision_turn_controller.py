a481e31d79e66dcb142fcfcee6f83ed656879869
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-06-15 10:27:09 +0800

    vision_turn_controller.py,目标横向加速度改为1.9

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -15,7 +15,7 @@ from openpilot.common.params import Params
 from openpilot.selfdrive.controls.lib.sunnypilot.helpers import debug
 from openpilot.common.logger import logger
 
-TARGET_LAT_A = 1.5  # m/s^2
+TARGET_LAT_A = 1.9  # m/s^2
 MIN_TARGET_V = 5  # m/s
 
 PARAMS_UPDATE_PERIOD = 5.
