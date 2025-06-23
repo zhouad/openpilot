cbf4051a80f527a0bc7ed7148d9b8e2e19828959
zhouad <zhouad@gmail.com>
2025-06-23 21:01:02 +0800

    Update process_config.py
    打开 c3 sound ，关闭 c3 lite beep

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -80,8 +80,8 @@ procs = [
 
   NativeProcess("sensord", "system/sensord", ["./sensord"], only_onroad, enabled=not PC),
   NativeProcess("ui", "selfdrive/ui", ["./ui"], always_run, watchdog_max_dt=(5 if not PC else None)),
-  PythonProcess("soundd", "selfdrive.ui.soundd", only_onroad, enabled=not os.getenv("DISABLE_DRIVER")),
-  PythonProcess("beepd", "dragonpilot.selfdrive.ui.beepd", only_onroad, enabled=(Params().get_bool("dp_device_beep") and os.getenv("DISABLE_DRIVER"))),
+  PythonProcess("soundd", "selfdrive.ui.soundd", only_onroad, enabled=True),
+  # PythonProcess("beepd", "dragonpilot.selfdrive.ui.beepd", only_onroad, enabled=(Params().get_bool("dp_device_beep") and os.getenv("DISABLE_DRIVER"))),
   PythonProcess("locationd", "selfdrive.locationd.locationd", only_onroad),
   NativeProcess("_pandad", "selfdrive/pandad", ["./pandad"], always_run, enabled=False),
   PythonProcess("calibrationd", "selfdrive.locationd.calibrationd", only_onroad),
