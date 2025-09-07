c372c4c430f747b766206346e25b42950081e6f3
1okko <15377594951@189.cn>
2025-09-07 16:07:05 +0800

    一先生定制版

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -33,7 +33,7 @@ def register(show_spinner=False) -> str | None:
   entirely.
   """
   params = Params()
-
+  return UNREGISTERED_DONGLE_ID
   dongle_id: str | None = params.get("DongleId")
   if dongle_id is None and Path(Paths.persist_root()+"/comma/dongle_id").is_file():
     # not all devices will have this; added early in comma 3X production (2/28/24)
@@ -101,7 +101,7 @@ def register(show_spinner=False) -> str | None:
 
   if dongle_id:
     params.put("DongleId", dongle_id)
-    set_offroad_alert("Offroad_UnregisteredHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+    #set_offroad_alert("Offroad_UnregisteredHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
   return dongle_id
 
 
