e9016545fe820cf67fb402e5fa6b68fa13519911
zhouad <zhouad@gmail.com>
2025-09-25 00:25:29 +0800

    Update launch_env.sh
    手动增加机型选择

--- launch_env.sh
+++ launch_env.sh
@@ -5,6 +5,10 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+# 增加
+export LITE=1
+export DISABLE_DRIVER=1
+
 if [ -s /data/params/d/dp_device_model_selected ]; then
   export FINGERPRINT="$(cat /data/params/d/dp_device_model_selected)"
 fi
