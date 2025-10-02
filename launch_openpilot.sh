8602edec8e592dde2188c7bfc6bf37f7e5852ff7
zhouad <zhouad@gmail.com>
2025-10-02 21:50:51 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -7,6 +7,6 @@ fi
 export API_HOST='https://api.konik.ai'
 export ATHENA_HOST='wss://athena.konik.ai'
 export MAPS_HOST=https://api.konik.ai/maps
-#export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 
 exec ./launch_chffrplus.sh
